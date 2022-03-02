8e6468c5873c9a09cf59dbd0ccd9b9454b301ff7
ThatGravyBoat <thatgravyboat@gmail.com>
2022-03-01 22:05:11 -0330

    Piggy Bank drops items

--- src/main/java/tech/thatgravyboat/playdate/common/blocks/custom/piggybank/PiggyBankPlushieBlock.java
+++ src/main/java/tech/thatgravyboat/playdate/common/blocks/custom/piggybank/PiggyBankPlushieBlock.java
@@ -1,11 +1,15 @@
 package tech.thatgravyboat.playdate.common.blocks.custom.piggybank;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
@@ -25,4 +29,27 @@ public class PiggyBankPlushieBlock extends PlushieBlock {
         }
         return InteractionResult.sidedSuccess(level.isClientSide);
     }
+
+    @Override
+    public void onRemove(BlockState state, @NotNull Level level, @NotNull BlockPos pos, BlockState newState, boolean moving) {
+        if (!state.is(newState.getBlock())) {
+            if (level.getBlockEntity(pos) instanceof PiggyBankPlushieBlockEntity entity) {
+                Containers.dropContents(level, pos, entity);
+                level.updateNeighbourForOutputSignal(pos, this);
+            }
+
+            super.onRemove(state, level, pos, newState, moving);
+        }
+    }
+
+    @Override
+    public boolean hasAnalogOutputSignal(@NotNull BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getAnalogOutputSignal(@NotNull BlockState state, Level level, @NotNull BlockPos pos) {
+        return AbstractContainerMenu.getRedstoneSignalFromBlockEntity(level.getBlockEntity(pos));
+    }
+
 }
