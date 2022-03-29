f25a42551ed66ed3524c62246c7906d82ecd48a0
ThatGravyBoat <thatgravyboat@gmail.com>
2022-03-29 21:29:20 -0230

    update descriptions and fix #1

--- common/src/main/java/tech/thatgravyboat/playdate/platform/services/IRegistryHelper.java
+++ common/src/main/java/tech/thatgravyboat/playdate/platform/services/IRegistryHelper.java
@@ -1,7 +1,5 @@
 package tech.thatgravyboat.playdate.platform.services;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
@@ -46,7 +44,6 @@ public interface IRegistryHelper {
 
     @FunctionalInterface
     interface ContainerFactory<T extends ScreenHandler> {
-        @Environment(EnvType.CLIENT)
         T create(int syncId, PlayerInventory inventory);
     }
 
